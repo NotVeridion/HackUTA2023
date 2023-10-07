67b91651e15bf3be1d83f6acfcf9ccd1ee38795b
Vincent Trinh <vincenttrinh65@gmail.com>
2023-10-07 17:01:10 -0500

    moved slb to navbar

--- HackUTA 2023/HackUTA2023/src/components/navbar.jsx
+++ HackUTA 2023/HackUTA2023/src/components/navbar.jsx
@@ -1,10 +1,11 @@
 import "./navbar.css"
-
+import ShoppingListButton from "./ShoppingListButton"
 function NavBar()
 {
     return (
         <nav class="navbar">
             <p>Navbar Working</p>
+            <ShoppingListButton></ShoppingListButton>
         </nav>
     )
 }
