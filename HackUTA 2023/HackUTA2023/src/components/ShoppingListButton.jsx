cf85c147e9e482bd059d85f1eca13ac284d9db04
Vincent Trinh <vincenttrinh65@gmail.com>
2023-10-07 16:25:38 -0500

    new changes in slb

---
+++ HackUTA 2023/HackUTA2023/src/components/ShoppingListButton.jsx
@@ -0,0 +1,11 @@
+
+
+function ShoppingListButton(){
+    return(
+        <button>
+            <img src="https://media.istockphoto.com/id/1206806317/vector/shopping-cart-icon-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=1RRQJs5NDhcB67necQn1WCpJX2YMfWZ4rYi1DFKlkNA="></img>
+        </button>
+    )
+}
+
+export default ShoppingListButton;
