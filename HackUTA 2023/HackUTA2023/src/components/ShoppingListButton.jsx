a8d3dad5b60ad5657bd0b7a280eed62dcb83b208
Matthew Nguyen <mdn8457@mavs.uta.edu>
2023-10-07 17:16:30 -0500

    navbar/shopping list update

--- HackUTA 2023/HackUTA2023/src/components/ShoppingListButton.jsx
+++ HackUTA 2023/HackUTA2023/src/components/ShoppingListButton.jsx
@@ -1,8 +1,11 @@
 import cart_img from "../assets/shopping_cart.jpg"
 import "./ShoppingListButton.css"
 function ShoppingListButton(){
+    function clickHandler(){
+        console.log("hello");
+    }
     return(
-        <button>
+        <button class="shoppingButton" onClick={clickHandler}>
             <img src={cart_img} alt="shopping cart"></img>
         </button>
     )
