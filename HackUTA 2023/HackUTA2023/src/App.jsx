87a68ea6cb124487ffc5a4eb0f85b52488b6e638
Vincent Trinh <vincenttrinh65@gmail.com>
2023-10-07 15:45:00 -0500

    added card wrapper to main

--- HackUTA 2023/HackUTA2023/src/App.jsx
+++ HackUTA 2023/HackUTA2023/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import NavBar from "./components/navbar"
-
+import Card from "./UI/Card"
 function App() {
     return (
+        <div>
         <NavBar></NavBar>
+        <Card></Card>
+        </div>
     )
 }
 
