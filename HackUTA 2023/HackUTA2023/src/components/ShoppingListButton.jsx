078de1c1ef9bb9a7ae18fd280aa6e74831b1e50a
Vincent Trinh <vincenttrinh65@gmail.com>
2023-10-07 16:38:52 -0500

    trying to add imgs

--- HackUTA 2023/HackUTA2023/src/components/ShoppingListButton.jsx
+++ HackUTA 2023/HackUTA2023/src/components/ShoppingListButton.jsx
@@ -1,9 +1,9 @@
-
+import cart_img from "./assets/shopping_cart.jpg"
 
 function ShoppingListButton(){
     return(
         <button>
-            <img src="https://media.istockphoto.com/id/1206806317/vector/shopping-cart-icon-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=1RRQJs5NDhcB67necQn1WCpJX2YMfWZ4rYi1DFKlkNA="></img>
+            <img src={cart_img}></img>
         </button>
     )
 }
