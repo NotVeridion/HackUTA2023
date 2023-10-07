3890867c597357c956730c6642615846323b3380
Anthony Fierro <anthonyfierro@Anthonys-Air.dhost.uta.edu>
2023-10-07 16:23:30 -0500

    added computers to list

--- HackUTA 2023/HackUTA2023/src/App.jsx
+++ HackUTA 2023/HackUTA2023/src/App.jsx
@@ -2,10 +2,39 @@ import { useState } from 'react'
 import NavBar from "./components/navbar"
 import Card from "./UI/Card"
 function App() {
+    
     return (
         <div>
+
         <NavBar></NavBar>
-        <Card></Card>
+        <Card>
+            <div class = "computers">
+                <div class = "card">
+                    <p class = "item">Macbook Air</p>
+                    <p>$999.99</p>
+                </div>
+                <div class = "card">
+                    <p class = "item">Macbook Pro</p>
+                    <p>$1399.99</p>
+                    <div class = "card">
+                    <p class = "item">HP Intel Core i3</p>
+                    <p>$849.99</p>
+                </div>
+                <div class = "card">
+                    <p class = "item">HP Chromebookr</p>
+                    <p>$649.99</p>
+                </div>
+                <div class = "card">
+                    <p class = "item">Lenovo Yoga 7i</p>
+                    <p>$749.99.99</p>
+                </div>
+                <div class = "card">
+                    <p class = "item">Dell XPS</p>
+                    <p>$1599.99</p>
+                </div>
+                </div>
+            </div>
+        </Card>
         </div>
     )
 }
