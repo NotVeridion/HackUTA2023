ceccbaa58ba9ab588e3988f3eea49f816a5d8fa3
Vincent Trinh <vincenttrinh65@gmail.com>
2023-10-07 23:03:50 -0500

    code cleanup and organziation

---
+++ HackUTA 2023/HackUTA2023/src/components/ItemDisplay.jsx
@@ -0,0 +1,12 @@
+import "./ItemDisplay.css"
+function ItemDisplay(props){
+
+    return(
+        <div class="card1">
+        <p class="title">{props.name}</p>
+        <img src={props.img} alt={props.name} style={{ width: '250px', height: '150px' }}/>
+        <p class = "price">{props.price}</p>
+      </div>
+    )
+}
+export default ItemDisplay;
