29f3ca9f23e2011d6bbdccfd36d5e9421a92bfd7
Anthony Fierro <anthonyfierro@Anthonys-Air.dhost.uta.edu>
2023-10-07 21:58:29 -0500

    changed website format

--- HackUTA 2023/HackUTA2023/src/App.jsx
+++ HackUTA 2023/HackUTA2023/src/App.jsx
@@ -28,97 +28,60 @@ function App() {
         </NavBar>
         <Card>
         <body>
-            <div class="container">
-
-                <div class="header">
-                <div class="logo">Ecomerence Logo</div>
-                <div class="menu">
-                <ul>
-                        <li>header link one</li>
-                        <li>header link two</li>
-                        <li>header link three</li>
-                        <li>header link four</li>
-                 </ul>
-                </div>
-            </div>
-
-            <div class="sidebar">
-            <div class="photo">
-                <p>placeholder for image</p>    
-            </div>
-            <div class="side-content">Box 1
-            </div>
-            <div class="side-content">Box 2
-            </div>
-            <div class="side-content">Box 3
-            </div>
-            </div>
-
-            <div class="nav">
-                <ul>
-                    <li>Latest Technology</li>
-                    <li>Most Viewed Technology</li>
-                    <li>Featured</li>
-                </ul>
-            </div>
-
-            <div class="computers">
-                <div class="card">
-                    <p class="title">Macbook Air</p>
-                    <img src={one} alt="Macbook img" style={{ width: '150px', height: '100px' }} />
-                    <p>$749.99</p> 
-                    
-                </div>
-                <div class="card">
-                    <p class="title">Macbook Pro M1</p>
-                    <img src={two} alt="Macbook Pro M1 img" style={{ width: '150px', height: '100px' }} />
-                    <p>$998.99</p> 
-
-                 </div>
-                <div class="card">
-                    <p class="title">Macbook Pro M2</p>
-                    <img src={three} alt="Macbook Pro M2 img" style={{ width: '150px', height: '100px' }} />
-                    <p>$1199.99</p> 
-
-                </div>
-                <div class="card">
-                     <p class="title">HP Chromebook Intel Celeron</p>
-                     <img src={four} alt="HP Chromebook img" style={{ width: '150px', height: '100px' }} />
-                     <p>$149.99</p> 
-
-                </div>
-                 <div class="card">
-                    <p class="title">Dell Inspiron</p>
-                    <img src={five} alt="Dell Inspiron img" style={{ width: '150px', height: '100px' }} />
-                    <p>$549.99</p> 
-
-                </div>
-                 <div class="card">
-                    <p class="title">HP Envy</p>
-                    <img src={six} alt="HP Envy img" style={{ width: '150px', height: '100px' }} />
-                    <p>$499.99</p> 
-
-                </div>
-                <div class="card">
-                    <p class="title">Lenovo Yoga 7i</p>
-                    <img src={seven} alt="Lenovo Yoga img" style={{ width: '150px', height: '100px' }} />
-                    <p>$899.99</p> 
-
-                </div>
-                <div class="card">
-                    <p class="title">HP AMD Ryzen 3</p>
-                    <img src={eight} alt="HP AMD img" style={{ width: '150px', height: '100px' }} />
-                    <p>$540.00</p> 
-
-                </div>
-            </div>
-
-      <div class="footer">
-        <p>E commis</p>
-      </div>
-      
+  <div class="container">
+    <div class="header">
+      <div class="logo">Computers</div>
     </div>
-  </body>
+
+    <div class="computers">
+      <div class="card">
+        <p class="title">Macbook Air</p>
+        <img src={one} alt="Macbook Air" style={{ width: '150px', height: '100px' }}/>
+        <p>$749.99</p>
+      </div>
+      <div class="card">
+        <p class="title">Macbook Pro M1</p>
+        <img src={two} alt="Macbook Pro M1" style={{ width: '150px', height: '100px' }}/>
+        <p>$998.99</p>
+      </div>
+      <div class="card">
+        <p class="title">Macbook Pro M2</p>
+        <img src={three} alt="Macbook Pro M2" style={{ width: '150px', height: '100px' }}/>
+        <p>$1199.99</p>
+      </div>
+      <div class="card">
+        <p class="title">HP Chromebook Intel Celeron</p>
+        <img src={four} alt="HP Chromebook Intel Celeron" style={{ width: '150px', height: '100px' }}/>
+        <p>$149.99</p>
+      </div>
+      <div class="card">
+        <p class="title">Dell Inspiron</p>
+        <img src={five} alt="Dell Inspiron" style={{ width: '150px', height: '100px' }}/>
+        <p>$549.99</p>
+      </div>
+      <div class="card">
+        <p class="title">HP Envy</p>
+        <img src={six} alt="HP Envy" style={{ width: '150px', height: '100px' }}/>
+        <p>$499.99</p>
+      </div>
+      <div class="card">
+        <p class="title">Lenovo Yoga 7i</p>
+        <img src={seven} alt="Lenovo Yoga 7i" style={{ width: '150px', height: '100px' }}/>
+        <p>$899.99</p>
+      </div>
+      <div class="card">
+        <p class="title">HP AMD Ryzen 3</p>
+        <img src={eight} alt="HP AMD Ryzen 3" style={{ width: '150px', height: '100px' }}/>
+        <p>$540.00</p>
+      </div>
+    </div>
+
+    <div class="footer">
+      <p>E commis</p>
+    </div>
+  </div>
+</body>
+
         </Card>
         </div>
         }
