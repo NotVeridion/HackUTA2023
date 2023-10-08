9ea8e368cef775f8753c38ea32dfeffb542ca8c3
“William” <wvt5303@mavs.uta.edu>
2023-10-08 01:25:21 -0500

    export webScraper

--- HackUTA 2023/HackUTA2023/src/webScraper.py
+++ HackUTA 2023/HackUTA2023/src/webScraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def readpage(topics_url, titles, cost ,image, links):
+def readpage(topics_url, titles, cost ,image, links, limit):
     
     response = requests.get(topics_url)
     page_info = response.text
@@ -16,10 +16,10 @@ def readpage(topics_url, titles, cost ,image, links):
     itemImage = doc.find_all("div", {"class" : "s-item__image-wrapper image-treatment"})
     itemLinks = doc.find_all("a", {"class" : "s-item__link"})
 
-    for tags in itemTitles[1:]:
+    for tags in itemTitles[1:limit]:
         titles.append(tags.text)
 
-    for tags in itemCost[1:]:
+    for tags in itemCost[1:limit]:
         grandchild = ""
         for child in tags.findChildren("div", {"class" : "s-item__detail s-item__detail--primary"} , recursive=False):
             if not grandchild:
@@ -30,11 +30,11 @@ def readpage(topics_url, titles, cost ,image, links):
         
         cost.append(grandchild[0].text)
 
-    for tags in itemImage[1:]:
+    for tags in itemImage[1:limit]:
         child = tags.findChildren("img" , recursive=False)
         image.append(child[0].get("src"))
 
-    for tags in itemLinks[1:]:
+    for tags in itemLinks[1:limit]:
         links.append(tags["href"])
     #print(cost)
     return True
@@ -43,34 +43,37 @@ def readpage(topics_url, titles, cost ,image, links):
 
 name = "iphone"
 page = 1
+limit = 12
 url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1311&_nkw=" + name + "&_pgn="+ str(page)
 titles = []
 costs = []
 images = []
 links = []
-url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1311&_nkw=" + name + "&_pgn="+ str(page)
-readpage(url, titles, costs, images, links)
+readpage(url, titles, costs, images, links, limit)
 #while(readpage(url, titles, costs, images, links)):
 #    url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p4432023.m570.l1311&_nkw=" + name + "&_pgn="+ str(page)
 #    page+=1
 
 
 ItemDict = {"title" : titles, "costs" : costs, "images" : images, "links" : links}
-df = pd.DataFrame(ItemDict)
-df.to_csv("topic.csv", index=None, header=None)
+#df = pd.DataFrame(ItemDict)
+#df.to_csv("topic.csv", index=None, header=None)
 for i in range(0, len(titles)):
     titles[i] = titles[i].replace('"', '\\\"')
     #print(titles[i])
 
 with open("Item.jsx", "w") as file:
-        file.write("const laptops = [\n")
+        file.write("function Item(){\n")
+        file.write("\tconst items = [\n")
         end = ""
         for i in range(0, len(titles)):
-            file.write("\t{\n")
-            file.write("\t\ttitle: \"" + titles[i] + "\",\n")
-            file.write("\t\tcosts: \"" + costs[i] + "\",\n")
-            file.write("\t\timages: \"" + images[i] + "\",\n")
-            file.write("\t\tlinks: \"" + links[i] + "\",\n")
-            file.write("\t},\n")
-        file.write("]")
+            file.write("\t\t{\n")
+            file.write("\t\t\ttitle: \"" + titles[i] + "\",\n")
+            file.write("\t\t\tcosts: \"" + costs[i] + "\",\n")
+            file.write("\t\t\timages: \"" + images[i] + "\",\n")
+            file.write("\t\t\tlinks: \"" + links[i] + "\",\n")
+            file.write("\t\t},\n")
+        file.write("\t]\n")
+        file.write("\treturn items\n}\n")
+        file.write("export default Item\n")
         
